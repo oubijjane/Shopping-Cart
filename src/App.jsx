8877f27d54ed581b5a750d718e4f23b02f10b9fd
oubijjane <79411385+oubijjane@users.noreply.github.com>
2025-10-22 15:58:11 +0100

    feat: add routing with react-router and create Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
+import { Link,Outlet } from "react-router";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
+import Home from './Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <NavBar />
+    <Outlet />
+  </>
   )
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/products/men">Men</Link></li>
+        <li><Link to="/products/women">Women</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      <ul>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+//import './index.css'
 import App from './App.jsx'
+import Home from './Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {path: "home", element: <Home />}
+    ]
+  },
+  {
+    path: "home",
+    element: <Home />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
